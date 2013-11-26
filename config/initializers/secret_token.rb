fb23beb09e298a6feacaa0236df8b88fef220fa3
wan <wantingyi@sina.com>
2013-11-26 21:46:15 +0800

    commit
    all

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BlogApp1::Application.config.secret_key_base = 'c871d7442ed63353b66c5994d80eb0ad742925697b3e5c341776fbf2bad6445d8e0ae31b377856bf84b9ad81a0df3fdd00208f3af3903e9d99030b427d5a4e21'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BlogApp1::Application.config.session_store :cookie_store, key: '_blog_app1_session'
